cf5ea94e0cf68f7379ffab192dc59e4b3a50d47f
Jay <zhao.j4@student.unimelb.edu.au>
2022-03-12 01:39:25 +1100

    Added Week2 code

--- src/main/java/au/edu/unimelb/ds/week2/UDPServer.java
+++ src/main/java/au/edu/unimelb/ds/week2/UDPServer.java
@@ -7,7 +7,7 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.io.IOException;
 
-public class udpServer {
+public class UDPServer {
 
     public static void main(String args[]) {
 
